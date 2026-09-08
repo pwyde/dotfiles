acb217494431b4d7900e65bf8e5899d274e09ea9
Patrik Wyde <patrik@wyde.se>
2026-09-08 09:51:54 +0200

    Fix eza icons with fzf-preview.

--- .config/zsh/conf.d/fzf-tab.zsh
+++ .config/zsh/conf.d/fzf-tab.zsh
@@ -1,5 +1,5 @@
 # Preview directory content with eza when completing cd.
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -hl --git --color=always --icons $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -hl --git --color=always --icons=auto $realpath'
 zstyle ':fzf-tab:*'             fzf-flags   --color="bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8" \
                                             --color="fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc" \
                                             --color="marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8" \
